8233b8cac7e321dc20cc370107df195a1cfff575
a71143055 <a71143055@gmail.com>
2025-06-28 22:52:37 +0900

    UAP

---
+++ src/main/java/kr/ac/kopo/jeong/useraccountproject/UserAccountProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.jeong.useraccountproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAccountProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAccountProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/kopo/jeong/useraccountproject/UserAccountProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.jeong.useraccountproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAccountProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
