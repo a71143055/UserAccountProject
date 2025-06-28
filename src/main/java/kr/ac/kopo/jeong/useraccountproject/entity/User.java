2e0abe48296b60c0d232165ad1cba9429bf647bf
a71143055 <a71143055@gmail.com>
2025-06-28 23:37:46 +0900

    UAP

--- src/main/java/kr/ac/kopo/jeong/useraccountproject/entity/User.java
+++ src/main/java/kr/ac/kopo/jeong/useraccountproject/entity/User.java
@@ -1,16 +1,13 @@
 package kr.ac.kopo.jeong.useraccountproject.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Getter
 @Setter
 @Entity
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
