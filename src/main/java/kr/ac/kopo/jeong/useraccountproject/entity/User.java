a98795c814c9184dadf49e0cf16102810c76e619
a71143055 <a71143055@gmail.com>
2025-06-28 23:18:30 +0900

    UAP

---
+++ src/main/java/kr/ac/kopo/jeong/useraccountproject/entity/User.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.jeong.useraccountproject.entity;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+}
+
