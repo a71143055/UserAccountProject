af636f30de1593cc9fa20b40ba57b695c840670a
a71143055 <a71143055@gmail.com>
2025-06-29 10:00:10 +0900

    UAP

--- src/main/java/kr/ac/kopo/jeong/useraccountproject/entity/User.java
+++ src/main/java/kr/ac/kopo/jeong/useraccountproject/entity/User.java
@@ -1,20 +1,14 @@
 package kr.ac.kopo.jeong.useraccountproject.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
     private String username;
-
     private String password;
-
     private String email;
 }
