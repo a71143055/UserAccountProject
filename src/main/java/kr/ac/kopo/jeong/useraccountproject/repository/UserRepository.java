f4ff3c156e8e82f9676c9aa46b6ab4254bd92fc7
a71143055 <a71143055@gmail.com>
2025-06-29 10:01:10 +0900

    UAP

--- src/main/java/kr/ac/kopo/jeong/useraccountproject/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package kr.ac.kopo.jeong.useraccountproject.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository<User> extends JpaRepository<User, Long> {
-    boolean existsByUsername(String username);
-}
